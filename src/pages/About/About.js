444f3308238b0c5723bb40006491f39d093bc5f9
Francisco Cuello <fcuello002@yahoo.com>
2022-08-19 15:08:17 -0400

    Cocktails

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,7 +4,7 @@ import './About.css'
 const About = () => {
   return (
     <section className="section about-section">
-      <h1 className="section-title">about us</h1>
+      <h1 className="section-title">about us...</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus quod
         molestiae tenetur iusto veritatis enim aperiam, minima officiis eveniet
